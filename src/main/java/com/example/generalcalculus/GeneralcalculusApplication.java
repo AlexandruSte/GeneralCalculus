e2007de04f8e52745cfba4fd3e76e88a90b17a49
Stefan Alexandru <alex.stefan007@gmail.com>
2019-04-22 20:39:09 +0300

    Initial commit

---
+++ src/main/java/com/example/generalcalculus/GeneralcalculusApplication.java
@@ -0,0 +1,13 @@
+package com.example.generalcalculus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralcalculusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneralcalculusApplication.class, args);
+    }
+
+}
