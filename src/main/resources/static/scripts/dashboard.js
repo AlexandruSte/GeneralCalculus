fa1f3b2933cc0c4cb6a920438c8c244c887a3989
Stefan Alexandru <alex.stefan007@gmail.com>
2019-04-27 22:30:10 +0300

    Coloring tables with jquery

--- src/main/resources/static/scripts/dashboard.js
+++ src/main/resources/static/scripts/dashboard.js
@@ -1,12 +1,4 @@
 function showTable(id){
-    /*var div = document.getElementById('tables');
-    var divs = div.getElementsByTagName('div');
-    for (var i = 0; i < divs.length; i += 1) {
-        divs[i].style.visibility = "hidden";
-        if(divs[i].id==id)
-            divs[i].style.visibility = "visible";
-    }*/
-
     $.ajax({
         type : "GET",
         url : "/dashboard/details",
@@ -17,7 +9,28 @@ function showTable(id){
             console.log(data);
             var div = document.getElementById('tables');
             div.innerHTML = data;
-            //window.location.href = "/success";
+
+            switch (id) {
+                case "additions":
+                    $( "tr:first" ).css( "background-color", "#00C853" );
+                    $("#second-table tr").eq(0).css("background-color", "#00C853");
+                    break;
+                case "multiplications":
+                    $( "tr:first" ).css( "background-color", "#0091EA" );
+                    $("#second-table tr").eq(0).css("background-color", "#0091EA");
+                    break;
+                case "subtractions":
+                    $( "tr:first" ).css( "background-color", "#6200EA" );
+                    $("#second-table tr").eq(0).css("background-color", "#6200EA");
+                    break;
+                case "divisions":
+                    $( "tr:first" ).css( "background-color", "#D50000" );
+                    $("#second-table tr").eq(0).css("background-color", "#D50000");
+                    break;
+            }
+
+            $( "tr:first" ).css( "color", "white" );
+            $("#second-table tr").eq(0).css("color", "white");
         },
         error : function(e) {
             alert('Error: ' + e);
