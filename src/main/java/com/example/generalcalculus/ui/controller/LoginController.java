b237a461c87b8a558eda4b29f0ac9e002a548560
Stefan Alexandru <alex.stefan007@gmail.com>
2019-04-28 17:00:48 +0300

    Update LoginController.java

--- src/main/java/com/example/generalcalculus/ui/controller/LoginController.java
+++ src/main/java/com/example/generalcalculus/ui/controller/LoginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value = "/user")
+@RequestMapping()
 public class LoginController {
 
     @GetMapping(value="/login")
