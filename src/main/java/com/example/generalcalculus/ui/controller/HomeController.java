6f50525e5f5e724d5f6f24d562b5d23e66927927
Stefan Alexandru <alex.stefan007@gmail.com>
2019-04-30 14:08:03 +0300

    Update HomeController.java

--- src/main/java/com/example/generalcalculus/ui/controller/HomeController.java
+++ src/main/java/com/example/generalcalculus/ui/controller/HomeController.java
@@ -1,13 +1,22 @@
 package com.example.generalcalculus.ui.controller;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
+@Scope("session")
 public class HomeController {
 
     @GetMapping(value = {"/", ""})
-    public String index(){
+    public String index(HttpSession session, ModelMap modelMap){
+        session.setMaxInactiveInterval(7200);
+        modelMap.put("id",session.getId());
+        modelMap.put("time",session.getMaxInactiveInterval());
+        modelMap.put("active",session.getCreationTime());
         return "index";
     }
 }
