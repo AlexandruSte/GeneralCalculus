6886479686f3e47673c10edbe225f9b4ae92e396
Stefan Alexandru <alex.stefan007@gmail.com>
2019-04-30 14:08:08 +0300

    Update CalculController.java

--- src/main/java/com/example/generalcalculus/ui/controller/CalculController.java
+++ src/main/java/com/example/generalcalculus/ui/controller/CalculController.java
@@ -1,8 +1,16 @@
 package com.example.generalcalculus.ui.controller;
 
+import com.example.generalcalculus.database.models.Score;
+import com.example.generalcalculus.database.models.User;
+import com.example.generalcalculus.database.repositories.ScoreRepository;
+import com.example.generalcalculus.database.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.time.LocalDate;
 
 @Controller
 @RequestMapping(value = "/calculus")
@@ -13,6 +21,12 @@ public class CalculController {
     private int correct = 0;
     private int total = 0;
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    ScoreRepository scoreRepository;
+
     @GetMapping(value = {"/",""})
     public String getIndex(){
         return "calculus";
@@ -20,7 +34,9 @@ public class CalculController {
 
     @PostMapping(value = "/results")
     public String controllerMethod(@RequestParam(value="myArray[]") String[] myArray,
-                                   @RequestParam(value="myArray1[]") String[] myArray1){
+                                   @RequestParam(value="myArray1[]") String[] myArray1,
+                                   @RequestParam(value="operations") String operation,
+                                   HttpSession session){
         int correctValues = 0;
         for(int i=0; i<myArray.length;i++)
             if(myArray[i].equals(myArray1[i]))
@@ -28,6 +44,11 @@ public class CalculController {
         correct = correctValues;
         total = myArray.length;
         modified = true;
+
+        Score score = new Score(total,correct,0,LocalDate.now(),"medium",operation,(User)session.getAttribute("user"));
+
+        scoreRepository.save(score);
+
         return "success";
     }
 
