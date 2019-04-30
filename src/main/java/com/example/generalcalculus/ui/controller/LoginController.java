57203a00cecaed1949761c1f77840f8e63923667
Stefan Alexandru <alex.stefan007@gmail.com>
2019-04-30 14:07:58 +0300

    Login done

--- src/main/java/com/example/generalcalculus/ui/controller/LoginController.java
+++ src/main/java/com/example/generalcalculus/ui/controller/LoginController.java
@@ -1,14 +1,22 @@
 package com.example.generalcalculus.ui.controller;
 
+import com.example.generalcalculus.database.models.User;
+import com.example.generalcalculus.database.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping()
+@Scope("session")
 public class LoginController {
+
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping(value="/login")
     public String login(){
@@ -16,7 +24,51 @@ public class LoginController {
     }
 
     @PostMapping(value = "/login")
-    public String loginP(){
-        return "succes";
+    public String loginP(@RequestParam(value="username") String username,
+                         @RequestParam(value="password") String password,
+                         ModelMap modelMap,
+                         HttpSession session){
+
+        User user = userRepository.findByUsername(username);
+
+        if(user == null){
+            modelMap.put("error","Nume de utilizator inexistent.");
+            return "register";
+        }
+
+        session.setAttribute("user",user);
+
+        return "redirect:/";
+    }
+
+    @GetMapping(value="/register")
+    public String register(){
+        return "register";
+    }
+
+    @PostMapping(value = "/register")
+    public String registerP(@RequestParam(value="username") String username,
+                            @RequestParam(value="password") String password,
+                            @RequestParam(value="cpassword") String confirmation,
+                            ModelMap modelMap){
+
+        User user = userRepository.findByUsername(username);
+
+        if(user != null){
+            modelMap.put("error","Acest nume de utilizator este indisponibil.");
+            return "register";
+        }
+
+        if(!password.equals(confirmation)){
+            modelMap.put("error","Parolele nu sunt compatibile.");
+            return "register";
+        }
+
+        user = new User();
+        user.setPassword(password);
+        user.setUsername(username);
+        userRepository.save(user);
+
+        return "redirect:/";
     }
 }
