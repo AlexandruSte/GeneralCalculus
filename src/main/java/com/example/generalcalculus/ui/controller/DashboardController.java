f4b54e6422202bc8f68f46fd57bba375aa28491a
Stefan Alexandru <alex.stefan007@gmail.com>
2019-04-30 14:08:12 +0300

    Update DashboardController.java

--- src/main/java/com/example/generalcalculus/ui/controller/DashboardController.java
+++ src/main/java/com/example/generalcalculus/ui/controller/DashboardController.java
@@ -1,12 +1,16 @@
 package com.example.generalcalculus.ui.controller;
 
 import com.example.generalcalculus.database.models.Score;
+import com.example.generalcalculus.database.models.User;
+import com.example.generalcalculus.database.repositories.ScoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -16,37 +20,29 @@ import java.util.List;
 @RequestMapping(value="/dashboard")
 public class DashboardController {
 
-    @GetMapping(value="")
+    @Autowired
+    ScoreRepository scoreRepository;
+
+    @GetMapping(value={"","/"})
     public String dashboard(){
         return "dashboard";
     }
 
     @GetMapping(value="/details")
     public String getResults(@RequestParam(value="operation") String operation,
-                             ModelMap modelMap){
+                             ModelMap modelMap,
+                             HttpSession session){
 
-        Score score = new Score(20,17,16.2,LocalDate.of(2019,4,27),"Expert",operation);
-        Score score1 = new Score(20,12,12.2, LocalDate.of(2019, Calendar.APRIL,27),"Expert",operation);
-
+        operation = operation.substring(0,operation.length()-1);
         List<Score> scores = new ArrayList<>();
-        scores.add(score);
-        scores.add(score);
-        scores.add(score);
-        scores.add(score);
-        scores.add(score);
-        scores.add(score);
-        scores.add(score);
-        scores.add(score);
+        User user = (User)session.getAttribute("user");
+        scores = scoreRepository.findByUserAndOperation(user,operation);
+        modelMap.put("scores",scores);
 
-        if(operation.equals("multiplications"))
-            scores.add(new Score(20,20,10,LocalDate.of(2019,2,22),"Medium",operation));
 
-        if(operation.equals("subtractions"))
-            modelMap.put("Scores",new ArrayList<Score>());
-        else{
-            modelMap.put("scores",scores);
-            modelMap.put("bestScores",scores);
-        }
+        scores = scoreRepository.findFirst10ByUserAndOperationOrderByAvgTimeDesc(user,operation);
+        modelMap.put("bestScores",scores);
+
         return "tableResults";
     }
 }
